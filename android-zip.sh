6f9813c0754e053d80fffd8653ec7f83a9afe8cf
Simon Mravec <simon.mravec@gmail.com>
2025-04-24 08:57:26 +0200

    Firefox fix

--- android-zip.sh
+++ android-zip.sh
@@ -1,1 +1,17 @@
-zip -r no-distractions.xpi manifest.json default_icon.png popup.js popup.html popup.css scripts css 
+# Create temporary directory
+mkdir -p temp_dir
+
+# Copy all files except manifest.json to temp directory
+cp default_icon.png popup.js popup.html popup.css temp_dir/
+cp -r scripts css temp_dir/
+
+# Copy manifest-firefox.json as manifest.json to temp directory
+cp manifest-firefox.json temp_dir/manifest.json
+
+# Create zip from temp directory
+cd temp_dir
+zip -r ../no-distractions.xpi *
+cd ..
+
+# Clean up
+rm -rf temp_dir
